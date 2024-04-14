d40ce2e9d53d8724531f1a718f0e6b38f153b7a7
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-04-14 15:59:15 +0200

    Added EventMapper with ToCommentDto and ToCommentFromCreateDto methods

---
+++ ScheduleMaker/Mappers/EventMapper.cs
@@ -0,0 +1,30 @@
+﻿using ScheduleMaker.DTO;
+using ScheduleMaker.Dtos;
+using ScheduleMaker.Models;
+
+namespace ScheduleMaker.Mappers;
+
+public static class EventMapper
+{
+    public static EventDto ToCommentDto(this Event eventModel)
+    {
+        return new EventDto
+        {
+            Id = eventModel.Id,
+            Title = eventModel.Title,
+            Description = eventModel.Discription,
+            StartTime = eventModel.startTime,
+            EndTime = eventModel.endTime
+        };
+    }
+    public static EventDto ToCommentFromCreateDto(this CreateEventRequestDto eventDto)
+    {
+        return new EventDto
+        {
+            Title = eventDto.Title,
+            Description = eventDto.Description,
+            StartTime = eventDto.StartTime,
+            EndTime = eventDto.EndTime
+        };
+    }
+}
