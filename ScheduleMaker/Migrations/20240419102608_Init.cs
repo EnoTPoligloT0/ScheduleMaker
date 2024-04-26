44311f8f91c4b6d7c7586d686089eeaeef0f886b
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-04-26 12:20:47 +0200

    Fixed main branch

---
+++ ScheduleMaker/Migrations/20240419102608_Init.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScheduleMaker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartYear = table.Column<int>(type: "int", nullable: false),
+                    StartMonth = table.Column<int>(type: "int", nullable: false),
+                    StartDay = table.Column<int>(type: "int", nullable: false),
+                    StartHour = table.Column<int>(type: "int", nullable: false),
+                    StartMinute = table.Column<int>(type: "int", nullable: false),
+                    EndYear = table.Column<int>(type: "int", nullable: false),
+                    EndMonth = table.Column<int>(type: "int", nullable: false),
+                    EndDay = table.Column<int>(type: "int", nullable: false),
+                    EndHour = table.Column<int>(type: "int", nullable: false),
+                    EndMinute = table.Column<int>(type: "int", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
--- ScheduleMaker/obj/Debug/net8.0/ScheduleMaker.AssemblyInfo.cs
+++ ScheduleMaker/obj/Debug/net8.0/ScheduleMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScheduleMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1201e3bf6e986865723311e444c47fda10e9c6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d9af3d10d04d6cd678b07b8a8bce63ae19205da")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScheduleMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScheduleMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
